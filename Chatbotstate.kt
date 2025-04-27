51e2267ca87dfeb54fb6cf566490c82653f49b0d
Ayush Kumar Gautam <157630163+ayushgautamm@users.noreply.github.com>
2025-04-28 00:01:30 +0530

    Delete Chatbotstate.kt

--- Chatbotstate.kt
+++
@@ -1,8 +0,0 @@
-package com.example.chatbot
-
-sealed class ChatBotuiState {
-    object Ideal : ChatBotuiState()
-    object Loading : ChatBotuiState()
-    data class Success(val chatData: String) : ChatBotuiState()
-    data class Error(val chatError: String) : ChatBotuiState()
-}
